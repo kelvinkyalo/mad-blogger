c739e784074c7fc6970f5b0ce92ec8b70d5fe5f2
joan-w <wamuyujoan4@gmail.com>
2020-05-10 20:40:41 +0300

    Routing the pages

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = '<h1>Hello Basic Bitches!! Fuck y\'all!! hahahahaha!!!!!!</h1>'
-    return title
+    title = 'Welcome'
+    return render_template('index.html', title=title)
+
+@app.route('/about')
+def about():
+    title = 'Welcome'
+    return render_template('about.html', title=title)
+
+@app.route('/post')
+def post():
+    title = 'Welcome'
+    return render_template('post.html', title=title)
+
+@app.route('/contact')
+def contact():
+    title = 'Welcome'
+    return render_template('contact.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
