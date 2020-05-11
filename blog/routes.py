e7a3181efa6af8b621370e7538096c086c0e8b94
joan-w <wamuyujoan4@gmail.com>
2020-05-11 15:12:21 +0300

    Updating the update_post route

--- blog/routes.py
+++ blog/routes.py
@@ -16,7 +16,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', posts=posts)
+    return render_template('about.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
