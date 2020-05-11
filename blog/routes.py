99ca51ba9714cd8be0b0c89b0f8818f73f6a2b89
joan-w <wamuyujoan4@gmail.com>
2020-05-11 15:15:38 +0300

    Editiing the db commit method

--- blog/routes.py
+++ blog/routes.py
@@ -127,6 +127,6 @@ def delete_post(post_id):
     if post.author != current_user:
         abort(403)
     db.session.delete(post)
-    db.commit()
+    db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
