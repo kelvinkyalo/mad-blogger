d5796f92f48cce10b490301a3f4f342e207c861f
joan-w <wamuyujoan4@gmail.com>
2020-05-11 05:06:16 +0300

    Adding a picture variable

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -45,16 +46,18 @@ class UpdateAccountForm(FlaskForm):
     email = StringField('Email',
                             validators=[DataRequired(),
                             Email()])
+    picture = FileField('Update profile picture',
+                            validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Chose another one.')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        if username.data != current_user.username:
+        if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Chose another one.')
+                raise ValidationError('That email is taken. Please choose a different one.')
