46757e15ebc59c180da10a3f12c808712fbc8cb8
joan-w <wamuyujoan4@gmail.com>
2020-05-12 02:11:07 +0300

    Adding blueprints

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from blog import db, login_manager, app
+from flask import current_app
+from blog import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -16,12 +17,12 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id' : self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']        
         except:
@@ -52,4 +53,4 @@ class Comment(db.Model):
     status = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return '<Comment %r>' % self.username
+        return f"User('{self.name}', '{self.message}')"
